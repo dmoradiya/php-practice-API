e8c54e35a10413627a678032581538da43dbf60c
dhmec <dharmoradiya@gmail.com>
2020-10-05 15:49:56 -0600

    initial commit; created index.php, header.php, navigation.php

---
+++ templates/navigation.php
@@ -0,0 +1,15 @@
+<nav>
+  <h2>Website Navigation</h2>
+  <ul>
+    <li>
+      <a href="index.php">
+        Home
+      </a>
+    </li>
+    <li>
+      <a href="api.php">
+        API data
+      </a>
+    </li>    
+  </ul>
+</nav>
