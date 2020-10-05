61ec6f5b228b6f55f69caf46ca0f759589b86c70
dhmec <dharmoradiya@gmail.com>
2020-10-05 16:56:18 -0600

    add form to search sybmol

--- api.php
+++ api.php
@@ -1,7 +1,7 @@
 <?php
 
 
-$GLOBALS['pageTitle'] = 'API Page';
+$GLOBALS['pageTitle'] = 'My Trading APP';
 
 // Show our header.
 include './templates/header.php';
@@ -12,6 +12,27 @@ include './templates/header.php';
   <?php echo $GLOBALS['pageTitle']; ?>
   page!
 </p>
+
+<!--Stock data Request Form--> 
+
+<h2>Get Stock Data Information</h2>
+<form action="#" method="POST">
+  <!-- <label for="amount">Enter the Amount of Facts:
+  <input type="number" id="amount" name="amount" value="3"></label> -->
+  <label for="company-symbol">Enter the Company Symbol:
+    <select id="company-symbol" name="symbol">
+      <option value="IBM">IBM</option>
+      <option value="MSFT">Microsoft</option>
+      <option value="AAPL">Apple</option>
+      <option value="AMZN">Amazon</option>
+      <option value="TSLA">Tesla</option>
+    </select>
+  </label>
+  <input type="submit" value="Search!">
+</form>
+
+
+
 
 
 
